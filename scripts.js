b6352892ec39f20c272dbf460827001709111afb
sofiapanchaud <114835397+sofiapanchaud@users.noreply.github.com>
2022-10-11 17:24:15 -0400

    implement basic modal functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+const app = {};
+const modal = $('.modal');
+const modalBtn = $('.modal-close');
+const openModal = $('.modal-open');
+
+app.modals = () => {
+    modalBtn.on('click', function(e){
+        modal.removeClass('is-open');
+    });
+    openModal.on('click', function(e){
+        e.preventDefault();
+        const matchId = $(this).attr('id');
+        $(`.${matchId}`).addClass('is-open');
+    })
+}
+
 $().ready(() => {
-    
+    app.modals();
 })
