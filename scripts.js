1b6595c5f150c7b145ee7ee12b78cca80b7f77f9
sofiapanchaud <114835397+sofiapanchaud@users.noreply.github.com>
2022-10-11 20:28:32 -0400

    Instructions modal prev & next functionality

--- scripts.js
+++ scripts.js
@@ -1,16 +1,41 @@
 const app = {};
-const modal = $('.modal');
-const modalBtn = $('.modal-close');
-const openModal = $('.modal-open');
 
 app.modals = () => {
-    modalBtn.on('click', function(e){
+    const modal = $('.modal');
+    const modalBtn = $('.modal-close');
+    const openModal = $('.modal-open');
+    const modalPane = $('.modal-inner_pane');
+    const pager = $('.modal-btn');
+    let page = 0; 
+    modalBtn.on('click', function(){
         modal.removeClass('is-open');
     });
     openModal.on('click', function(e){
         e.preventDefault();
         const matchId = $(this).attr('id');
         $(`.${matchId}`).addClass('is-open');
+    });
+    pager.on('click', function(){
+        if ($(this).hasClass('next')){
+            $(modalPane[page]).addClass('hidden');
+            page = page + 1;
+            $(modalPane[page]).removeClass('hidden');
+            if (page === modalPane.length - 1){
+                $(this).attr('disabled', true);
+            } 
+            
+        } else  {
+            $(modalPane[page]).addClass('hidden');
+            page = page - 1;
+            $(modalPane[page]).removeClass('hidden');
+            if (page === 0) {
+                $(this).attr('disabled', true);
+            }
+        }
+        console.log(page);
+        if (page !== 0 && page !== modalPane.length - 1){
+            pager.attr('disabled', false);
+        }
     })
 }
 
